d3cfdd11ff35f91826dd9c742338261bbe772b5e
soloiaros <soloiaros@gmail.com>
2026-02-03 21:35:19 +0300

    Added hero section and header Also, the reveal of the image was made a bit smoother, animation for the event of unhovering was added. For the hero section, a blobby svg outline was added and animated

--- hover-reveal.js
+++ hover-reveal.js
@@ -3,6 +3,13 @@ const imageArea = document.querySelector(".background-image");
 const circle = document.getElementById("circle");
 
 imageArea.addEventListener("mousemove", (event) => {
+  circle.setAttribute("r", 250);
   circle.setAttribute("cx", event.offsetX);
   circle.setAttribute("cy", event.offsetY);
+});
+
+imageArea.addEventListener("mouseleave", (event) => {
+  circle.setAttribute("r", 0);
+  circle.setAttribute("cx", imageArea.offsetWidth - 500);
+  circle.setAttribute("cy", imageArea.offsetHeight - 500);
 });
